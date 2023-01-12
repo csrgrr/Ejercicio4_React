3f57cd38f181de9792adc741e5d0a68ad2835830
csrgrr <cesar.guerra@softwareone.com>
2023-01-12 09:51:18 +0100

    cambios

--- src/FunctionComponent.jsx
+++ src/FunctionComponent.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 import VariableComponent from './VariableComponent'
 
 let texto = VariableComponent()
+function generadorDeText(textoAEscribir){
+    return textoAEscribir
+}
 function FunctionComponent() {
 
     return(
-        <h1>{texto}</h1>
+        <h1>{generadorDeText(texto)}</h1>
     )
 }
 
